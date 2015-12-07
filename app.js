9b56ff8bc9902e5d5a2c1953ddf35e1a58d671cd
Fabien Tesselaar <fabientesselaar@gmail.com>
2015-12-07 21:48:24 +0100

    JSON stringify for result

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+app.set("port", (process.env.PORT || 5000));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-var bot = require('./bot');
-app.post('/hello', bot);
+
+app.post("/", function (req, res, next) {
+    var userName = req.body.user_name || "you";
+    var payload = {
+      text : "Hello, " + userName + "!"
+    };
+
+    // avoid infinite loop
+    if (userName !== "slackbot") {
+        var payload = {
+            "text": "http://currencybot.github.io/CurrencyBot.png"
+        };
+
+        res.send(JSON.stringify(payload));
+        res.end();
+    }
+    else {
+        return res.status(200).end();
+    }
+});
+
 
 // error handler
 app.use(function (err, req, res, next) {
@@ -16,5 +35,5 @@ app.use(function (err, req, res, next) {
 });
 
 app.listen(app.get("port"), function () {
-  console.log('Slack bot listening on port', app.get("port"));
+  console.log("Slack bot listening on port", app.get("port"));
 });
