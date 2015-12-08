ed07da912bb3062950e28122dabf25faa8b4b565
Fabien Tesselaar <fabientesselaar@gmail.com>
2015-12-08 09:11:44 +0100

    Random get url?

--- app.js
+++ app.js
@@ -72,17 +72,12 @@ app.post("/images", function (req, res, next) {
 
         if (best_match.score > 0) {
             slack.send({
-                text: "",
+                text: app.get("base") + _.sample(best_image.img) + "?id=" + new Date().getTime() + "\n" + req.body.text,
                 channel: '#' + (req.body.channel_name || "random"),
                 username: 'Sticker',
                 icon_emoji: ':' + _.sample(emoji) + ':',
-                attachments: [
-                    {
-                        "fallback": req.body.text,
-                        "title": req.body.text,
-                        "image_url": app.get("base") + _.sample(best_image.img)
-                    }
-                ],
+                unfurl_links: true,
+                link_names: 1
             });
         }
     }
